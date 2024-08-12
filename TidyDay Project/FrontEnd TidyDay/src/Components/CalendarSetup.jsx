13b928c885981a450f79dc8df558554d82222ee8
johancruiz <106390573+johancruiz@users.noreply.github.com>
2024-08-12 11:23:24 -0500

    Change of calendar

--- TidyDay Project/FrontEnd TidyDay/src/Components/CalendarSetup.jsx
+++ TidyDay Project/FrontEnd TidyDay/src/Components/CalendarSetup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
@@ -6,7 +6,14 @@ import moment from "moment";
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState(() => {
+    const storedEvents = localStorage.getItem("events");
+    return storedEvents ? JSON.parse(storedEvents) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
 
   const handleSelect = ({ start, end }) => {
     const title = window.prompt("New Event name");
@@ -16,7 +23,7 @@ const MyCalendar = () => {
         end,
         title,
       };
-      setEvents([...events, newEvent]);
+      setEvents((prevEvents) => [...prevEvents, newEvent]);
     }
   };
 
@@ -30,6 +37,8 @@ const MyCalendar = () => {
         style={{ height: 500 }}
         selectable
         onSelectSlot={handleSelect}
+        views={['month', 'agenda']} // Mantener solo mes y agenda
+        defaultView="month" // Vista predeterminada
       />
     </div>
   );
